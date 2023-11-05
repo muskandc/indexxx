297022871adcfab0240fbeca7cb02916390f1dbe
muskandc <147479650+muskandc@users.noreply.github.com>
2023-11-05 02:33:34 -0800

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,10 @@
+  function first (){
+            document.getElementById("slideimage").src="images/main1.png";
+        }
+          function two () {
+                document.getElementById("slideimage").src ="images/main2.png";
+            }
+          function three () {
+                document.getElementById("slideimage").src ="images/main1.png";
+            }    
+        
